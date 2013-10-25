76d3ee452b6b04327181cc6bf656203efa1781bf
zhangran <529130510@qq.com>
2013-10-25 18:06:44 +0800

    add mvvm

--- src/mvvm.js
+++ src/mvvm.js
@@ -5,7 +5,7 @@ define([url_prefix+'data/data',
 	url_prefix+'event/domReady',
 	url_prefix+'sizzle/sizzle',
 	url_prefix+'style/style',
-	'../lib/utils/utils'],function(data,animate,events,domReady,sizzle,style){
+	'../lib/utils/utils'],function(data,animate,events,domReady,sizzle,style,utils){
 	
 	var mvvm = {}
 	//全局配置
@@ -15,21 +15,180 @@ define([url_prefix+'data/data',
 	var window = this || (0,eval)('this'),
 	document = window.document;
 
+	//一个简单的观察者
+	//For details, see http://www.cnblogs.com/tomxu/archive/2012/03/02/2355128.html
+	var pubsub = {};
+	(function (q) {
+
+	    var topics = {}, // 回调函数存放的数组
+	        subUid = -1;
+	    // 发布方法
+	    q.publish = function (topic, args) {
+	    	console.log(topics)
+	        if (!topics[topic]) {
+	            return false;
+	        }
+
+	        setTimeout(function () {
+	            var subscribers = topics[topic],
+	                len = subscribers ? subscribers.length : 0;
+
+	            while (len--) {
+	                subscribers[len].func(topic, args);
+	            }
+	        }, 0);
+
+	        return true;
+
+	    };
+	    //订阅方法
+	    q.subscribe = function (topic, func) {
+
+	        if (!topics[topic]) {
+	            topics[topic] = [];
+	        }
+
+	        var token = (++subUid).toString();
+	        topics[topic].push({
+	            token: token,
+	            func: func
+	        });
+	        console.log(topics)
+	        return token;
+	    };
+	    //退订方法
+	    q.unsubscribe = function (token) {
+	        for (var m in topics) {
+	            if (topics[m]) {
+	                for (var i = 0, j = topics[m].length; i < j; i++) {
+	                    if (topics[m][i].token === token) {
+	                        topics[m].splice(i, 1);
+	                        return token;
+	                    }
+	                }
+	            }
+	        }
+	        return false;
+	    };
+	} (pubsub));
 	/*
 	vm:object 绑定对象
 	rootNode:根节点
 	 */
-	mvvm.bind = function(vm,rootNode){
-		if(!(rootNode && /1|9/i.test(rootNode.nodeType))){
-			rootNode  = document.body || document.documentElement;
-		}
-
-		var doms = getTriggerDoms(rootNode)
-		console.log(doms)
+	mvvm.applyBindings = function(vm,rootNode){
+		domReady(function(){
+			if(!(rootNode && /1|9/i.test(rootNode.nodeType))){
+				rootNode  = document.body || document.documentElement;
+			}
+			
+			var domsAndAttrs = getTriggerDoms(rootNode);
+			analysisBindRulers(vm,domsAndAttrs);
+		})
 	}
 
+
+	mvvm.observable = function(val){
+		return function(newVal,currentDom,random){
+			pubsub.publish(random,[newVal || val,currentDom]);
+		}
+	}   
+	/*
+	取得所有符合条件节点
+	 */
 	function getTriggerDoms(rootNode){
-		return sizzle('*['+ mvvm.trigger +']',rootNode)
+		//return sizzle('*['+ mvvm.trigger +']',rootNode)
+		var domLocateds = rootNode.getElementsByTagName('*'),
+			i = 0,
+			len = domLocateds.length,
+			domAttrName,
+			dataBindDomLists = [],
+			currentDom;
+		for(;i<len;i++){
+			currentDom = domLocateds[i];
+			domAttrName = currentDom.getAttribute(mvvm.trigger);
+			if(domAttrName == undefined){
+				domAttrName = currentDom[mvvm.trigger];
+			}
+
+			if( domAttrName != undefined) {
+				dataBindDomLists.push({
+					dom:currentDom,
+					attr:domAttrName
+				});
+			}
+		}
+		domLocateds = null;
+		return dataBindDomLists;
+	}
+
+	/*
+	分析绑定规则
+	 */
+	var bindPattern = /^\s*(text|visible|html|css|style|attr)\s*:\s*(.+)\s*$/;
+	var bindRoute = {
+		text:routeTextFn
+	}
+	/*
+	domsAndAttrs:[]
+	 */
+	function analysisBindRulers(vm,domsAndAttrs){
+		var i = 0,
+			len = domsAndAttrs.length,
+			execAry,
+			currentDom,
+			currentAttr,
+			cuurentFn;
+		if(len === i) return;
+		for(;i<len;i++){
+			currentDom = domsAndAttrs[i].dom;
+			currentAttr = domsAndAttrs[i].attr;
+			execAry = bindPattern.exec(currentAttr);
+			if(execAry && execAry.length>=3){
+				//分发出去
+				//console.log(execAry)
+				var text = execAry[2];
+				text = utils.trim(text);
+				var type = utils.getType(vm[text]);
+				var value;
+				var random = getMandom();
+				cuurentFn = bindRoute[execAry[1]];
+				
+				if(type === 'String'){
+					value = vm[text];
+					cuurentFn(null,[value,currentDom]);
+				}else if(type === 'Function'){
+					pubsub.subscribe(random,cuurentFn)
+					vm[text](undefined,currentDom,random);					
+				}
+				
+			}
+		}
+	}
+
+
+	//text绑定
+	function routeTextFn(isNull,ary){
+		console.log(ary)
+		setText(ary[1],ary[0]);
+	}
+
+
+	//################################
+	//常用方法
+	function getMandom(){
+		return new Date().getTime();
+	}
+
+
+
+	//############################################################
+	//dom操作的常用方法
+	function setText(dom,text){
+		if(dom.textContent){
+			dom.textContent = text;
+		}else if(dom.innerText){
+			dom.innerText = text;
+		}
 	}
 
 	return mvvm;
