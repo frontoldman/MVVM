8d4b0f54a1167827635c6ed9c58746f7cf6484ea
Zhang Ran <zrlakey123@gmail.com>
2013-10-29 21:32:08 +0800

    add basic attrs

--- src/mvvm.js
+++ src/mvvm.js
@@ -11,6 +11,10 @@ define([url_prefix+'data/data',
 	//全局配置
 	mvvm.trigger = 'data-bind';
 
+	var dataCache = {
+		css:'mvvm-css',
+		visible:'mvvm-visible'
+	};
 	// For details, see http://stackoverflow.com/questions/14119988/return-this-0-evalthis/14120023#14120023
 	var window = this || (0,eval)('this'),
 	document = window.document;
@@ -97,11 +101,12 @@ define([url_prefix+'data/data',
 			if(random){
 				_random.push(random);
 			}
-			var  i = bindLast ? _random.length-1:0;//初始化绑定只绑定最后一个
+			var i = bindLast ? _random.length-1:0,//初始化绑定只绑定最后一个
+				observableVal = newVal === undefined ? val : newVal;
 			for(var i = 0;i<_random.length;i++){
-				pubsub.publish(_random[i],[newVal || val,_currentDom[i]]);
+				pubsub.publish(_random[i],[observableVal,_currentDom[i]]);
 			}
-			
+			return observableVal;
 		}
 	}   
 	
@@ -138,7 +143,11 @@ define([url_prefix+'data/data',
 	
 	var bindRoute = {
 		text:routeTextFn,
-		css:routeCssFn
+		css:routeCssFn,
+		html:routeHtmlFn,
+		visible:routeVisibleFn,
+		style:routeStyleFn,
+		attr:routeAttrFn
 	}
 	
 	
@@ -176,24 +185,22 @@ define([url_prefix+'data/data',
 			}
 			
 			attrsValueObject = _fn.bind(vm)(vm);
-			console.log(attrsValueObject)
+			//console.log(attrsValueObject)
 			//console.log(vm)
 			for(bindKey in attrsValueObject){
 				vmValue = attrsValueObject[bindKey];
 				type = utils.getType(vmValue);
 				currentFn = bindRoute[bindKey] ? bindRoute[bindKey]
 								: function(){};
-				
+				//console.log(type)
 				switch(type){
-					case 'String':
-						currentFn([vmValue,currentDom]);
-						break;
 					case 'Function':
 						random = getRandom();
 						pubsub.subscribe(random,currentFn);
 						vmValue(undefined,currentDom,random,true);
 						break;
 					default:
+						currentFn([vmValue,currentDom]);
 						break;
 				}
 			}	
@@ -201,95 +208,7 @@ define([url_prefix+'data/data',
 	}
 	
 	 
-	 /**
-	function analysisBindRulers(vm,domsAndAttrs){
-		var i = 0,
-			len = domsAndAttrs.length,
-			execAry,
-			currentDom,
-			currentAttr,
-			cuurentFn;
 
-		for(;i<len;i++){
-			currentDom = domsAndAttrs[i].dom;
-			currentAttr = domsAndAttrs[i].attr;
-			bindPattern = new RegExp(mainBindPatternStr,'g');
-			execAry = bindPattern.exec(currentAttr);
-	
-			while(execAry && execAry.length>=3){
-				//分发出去
-				//console.log(execAry)
-				var text = execAry[2];
-				text = utils.trim(text);
-				var value = vm[text],
-					random,
-					type;
-				cuurentFn = bindRoute[execAry[1]] ? bindRoute[execAry[1]]
-									: function(){};
-				
-				if(value){
-					type = utils.getType(value);
-					
-					switch(type){
-						case 'String':
-							cuurentFn([value,currentDom]);
-							break;
-						case 'Function':
-							random = getRandom();
-							pubsub.subscribe(random,cuurentFn);
-							value(undefined,currentDom,random,true);
-							break;
-						default:
-							break;
-					}
-				}else{					
-					var varStr = convertVariableScope(vm);
-					var fn ;
-					//console.log(text)
-					try{
-						fn = new Function(varStr + 'return '  + text);
-					}catch(e){
-						throw 'Wrong expressions!';
-					}
-					console.log(fn);
-					vm[text] = fn;
-					//console.log()
-					//fn.bind(vm);
-					//var __fn = fn.apply(vm);
-					//console.log(__fn);
-					value = vm[text]();
-					//console.log(value)
-					cuurentFn([value,currentDom])
-				}
-				
-				execAry = bindPattern.exec(currentAttr);
-			}
-		}
-	}
-	**/
-	
-	// function convertVariableScope(vm){
-	// 	var varStr = '',
-	// 	value,
-	// 	flag,
-	// 	quotes ,
-	// 	type;
-	// 	for(var i in vm){
-	// 		value = vm[i];
-	// 		type = utils.getType(value);
-	// 		if(type === 'String'){
-	// 			if(/'/.test(value)){
-	// 				value = '"' + value + '"';
-	// 			}else{
-	// 				value = "'" + value + "'";
-	// 			}
-	// 		}else {
-	// 			value = value.toString();
-	// 		}
-	// 		varStr += ('var ' + i +' = ' + value + ';\n');
-	// 	}
-	// 	return varStr;
-	// }
 
 	//text绑定
 	//ary:[text,dom]
@@ -301,6 +220,46 @@ define([url_prefix+'data/data',
 	//ary:[className,dom]
 	function routeCssFn(ary){
 		setClassName(ary[1],ary[0]);
+	}
+
+	//html绑定
+	//ary:[html,dom]
+	function routeHtmlFn(ary){
+		var dom = ary[1],
+			html = ary[0];
+		dom.innerHTML = html;
+	}
+
+	//显示与隐藏
+	function routeVisibleFn(ary){
+		var dom = ary[1],
+			visible = ary[0];
+		//console.log(visible)
+		var display = visible ? '':'none';
+		//console.log('display:'+ display)
+		dom.style.display = display;
+	}
+
+	//style
+	function routeStyleFn(ary){
+		var dom = ary[1],
+			styleObj = ary[0];
+
+		style.set(dom,styleObj);
+	}
+
+	//attr
+	function routeAttrFn(ary){
+		var dom = ary[1],
+			attrObj = ary[0],
+			attrKey;
+
+		if(utils.getType(attrObj) 	=== 'Object'){
+			for(attrKey in attrObj){
+				console.log(attrObj)
+				dom.setAttribute(attrKey,attrObj[attrKey]);
+			}
+		}
 	}
 	//################################
 	//常用方法
@@ -321,9 +280,23 @@ define([url_prefix+'data/data',
 	}
 	
 	function setClassName(dom,className){
-		
+
+		var mvvmCss = data(dom,dataCache.css),
+			oldClassName = dom.className,
+			newClassName;
+		if(mvvmCss){
+			newClassName = oldClassName.replace(mvvmCss,className)
+		}else{
+			newClassName = oldClassName ? oldClassName + ' ' + className : className;
+		}
+		dom.className = newClassName;
+		data(dom,dataCache.css,className);
 	}
 	
+	function removeClassName(dom,className){
+
+	}
+
 	//############################################################
 	//扩展方法
 	//from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
